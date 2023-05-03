133c54d8f079e176aef04f6536d63f151b57f873
pr0m3th3usEx <thomas1.michel@epitech.eu>
2023-05-03 16:48:32 -0700

    init commit

---
+++ TapHap/TapHapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapHapApp.swift
+//  TapHap
+//
+//  Created by pr0m3th3usEx on 03/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct TapHapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
